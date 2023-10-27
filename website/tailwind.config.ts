e4e599e6b7f5b314c2ead9d97ddafeb5909baee8
Jakub Czarlinski <jakubczarlinski@gmail.com>
2023-10-27 01:08:25 +0100

    Add quicklinks and logo animation.

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -8,12 +8,26 @@ const config: Config = {
       current: "currentColor",
       black: "#08121f",
       white: "#efefef",
-      gray: "#3c3c3c",
+      "wai-gray": "#3c3c3c",
       purple: "#6D60C1",
       lavender: "#A6A6E2",
       custard: "#F7F1AA",
       "blue-green": "#008E93",
       "dull-blue-green": "#C4DDDC",
+    },
+    extend: {
+      keyframes: {
+        colourPulse: {
+          "0%, 100%": {
+            "stop-color": "#3c3c3c",
+          },
+          "50%": { "stop-color": "#6D60C1" },
+        },
+      },
+      animation: {
+        colourPulse: "colourPulse 10s ease-in-out infinite",
+        colourPulseOffset: "colourPulse 10s ease-in-out infinite -5s",
+      },
     },
   },
   plugins: [],
