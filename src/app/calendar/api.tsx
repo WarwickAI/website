1082d00d647c7058aad604e609970c01bc205dc6
Jakub Czarlinski <jakubczarlinski@gmail.com>
2023-10-30 00:55:30 +0000

    Add support for calendar API keys.

--- src/app/calendar/api.tsx
+++ src/app/calendar/api.tsx
@@ -6,11 +6,9 @@ import FullCalendar from "@fullcalendar/react";
 
 import React from "react";
 
-// TODO(czarlinski): Replace with env var.
-const apiKey = "API_KEY";
-const calendarId = "CALENDAR_ID";
+const apiKey = process.env.GOOGLE_CAL_API_KEY || "FAKE_KEY";
+const calendarId = process.env.GOOGLE_CALENDAR_ID || "FAKE_ID";
 
-// TODO(czarlinski): Style the calendar.
 export default class Calendar extends React.Component {
   render() {
     return (
@@ -18,19 +16,10 @@ export default class Calendar extends React.Component {
         viewClassNames={["w-full h-full"]}
         plugins={[googleCalendarPlugin, listPlugin]}
         initialView="list"
-        // googleCalendarApiKey={apiKey}
-        // events={{
-        //   googleCalendarId: calendarId,
-        // }}
-        events={[
-          { title: "IDK bro", date: "2023-10-28" },
-          { title: "XSOC Python Course", date: "2023-10-29" },
-          {
-            title: "Code Night",
-            start: "2023-10-29T14:30",
-            end: "2023-10-28T17:30",
-          },
-        ]}
+        googleCalendarApiKey={apiKey}
+        events={{
+          googleCalendarId: calendarId,
+        }}
         noEventsContent={function () {
           return (
             <div className="text-center">
