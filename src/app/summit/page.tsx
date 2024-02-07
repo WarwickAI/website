c77961d01587d2a7569dab3d5433933d42205823
JakubCzarlinski <jakubczarlinski@gmail.com>
2024-02-07 13:10:00 +0000

    Add summit ticket redirection.

---
+++ src/app/summit/page.tsx
@@ -0,0 +1,10 @@
+// Summit page for tickets
+// TODO(JakubCzarlinski): Remove after the event.
+
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  return redirect(
+    "https://www.warwicksu.com/venues-events/events/57846/25060/"
+  );
+}
