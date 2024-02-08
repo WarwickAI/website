fb0c7bf5cac882963d4ce6f67d922ef50ffbe766
Jakub Czarlinski <jakubczarlinski@gmail.com>
2024-02-08 16:55:42 +0000

    Remove FullCalendar package.

--- src/components/calendar/google_api.tsx
+++ src/components/calendar/google_api.tsx
@@ -1,5 +1,4 @@
 import { CalendarEvent } from "@/components/calendar/list_calendar";
-import { DateInput, EventInput } from "@fullcalendar/core/index.js";
 import { GaxiosPromise, calendar_v3 } from "@googleapis/calendar";
 import { google } from "googleapis";
 
@@ -7,32 +6,15 @@ const apiKey = process.env.GOOGLE_CAL_API_KEY || "FAKE_KEY";
 const calendarId = process.env.GOOGLE_CAL_ID || "FAKE_ID";
 const googleCalendar = google.calendar({ version: "v3", auth: apiKey });
 
-// We rate limit the calendar API to 1 new request every minute.
+// Refresh events every 5 minutes minus 10 seconds to never force a user to wait.
 var lastUpdated = new Date();
 const rateLimitMinutes = 5;
 lastUpdated.setFullYear(1970); // Force a refresh on first load.
-
-// Refresh events every 5 minutes minus 10 seconds to never force a user to wait.
-type FullCalendarEvent = {
-  id: string;
-  title: string;
-  start: string;
-  end: string;
-  url: string | undefined;
-  extendedProps: {
-    description: string | null | undefined;
-    location: string | null | undefined;
-  };
-  description: string | null | undefined;
-};
-
 var events: CalendarEvent[] = [];
 setInterval(forceLoadEvents, 60000 * rateLimitMinutes - 10000);
 
 export async function loadEvents(): Promise<CalendarEvent[]> {
-  // Loads events from Google Calendar API with a rate limit.
-
-  // If we've already loaded events in the last 5 minutes, return the cached events.
+  // Loads events from Google Calendar API with a cache/rate limit.
   const now = new Date();
   const diff = now.getTime() - lastUpdated.getTime();
   const diffMinutes = Math.floor(diff / 60000);
@@ -42,7 +24,6 @@ export async function loadEvents(): Promise<CalendarEvent[]> {
     return events;
   }
 
-  // Otherwise, load events from Google Calendar API.
   const result = await forceLoadEvents();
   return result;
 }
@@ -50,11 +31,6 @@ export async function loadEvents(): Promise<CalendarEvent[]> {
 async function forceLoadEvents(): Promise<CalendarEvent[]> {
   console.log("Loading events from Google Calendar API.");
   const response = await getEventsFromGoogle();
-  if (!response) {
-    console.log("No response from API.");
-    events = [];
-    return events;
-  }
   if (response.status !== 200) {
     console.log("API returned status code: " + response.status);
     events = [];
@@ -62,21 +38,14 @@ async function forceLoadEvents(): Promise<CalendarEvent[]> {
   }
 
   lastUpdated = new Date();
-  if (!response.data.items) {
+  if (!response.data.items || response.data.items.length === 0) {
     console.log("No upcoming events found.");
     events = [];
     return events;
   }
 
-  const googleCalendarEvents = response.data.items;
-  if (googleCalendarEvents.length) {
-    events = converListToFullCalendarEvents(googleCalendarEvents);
-    return events;
-  } else {
-    console.log("No upcoming events found.");
-    events = [];
-    return events;
-  }
+  events = convertToCalendarEvents(response.data.items);
+  return events;
 }
 
 async function getEventsFromGoogle(): Promise<
@@ -95,7 +64,7 @@ async function getEventsFromGoogle(): Promise<
 }
 
 function convertGoogleToFullCalendarEvent(
-  googleCalendarEvent: calendar_v3.Schema$Event
+  googleCalendarEvent: calendar_v3.Schema$Event,
 ): CalendarEvent {
   // Converts a single event from the Google Calendar API to a FullCalendar API.
   if (!googleCalendarEvent.start) {
@@ -107,29 +76,23 @@ function convertGoogleToFullCalendarEvent(
   if (!googleCalendarEvent.id) {
     throw new Error("No id found on event.");
   }
-  const start: DateInput = googleCalendarEvent.start.dateTime || "";
-  const end: DateInput = googleCalendarEvent.end.dateTime || "";
-  const title: string = googleCalendarEvent.summary || "";
-  const url = googleCalendarEvent.htmlLink || undefined;
-  const location = googleCalendarEvent.location || undefined;
-  const description = googleCalendarEvent.description || undefined;
   return {
     id: googleCalendarEvent.id,
-    title: title,
-    start: start,
-    end: end,
-    url: url,
-    location: location,
-    description: description,
+    title: googleCalendarEvent.summary || "",
+    start: googleCalendarEvent.start.dateTime || "",
+    end: googleCalendarEvent.end.dateTime || "",
+    url: googleCalendarEvent.htmlLink || undefined,
+    location: googleCalendarEvent.location || undefined,
+    description: googleCalendarEvent.description || undefined,
   };
 }
 
-function converListToFullCalendarEvents(
+function convertToCalendarEvents(
   googleCalendarEvents: calendar_v3.Schema$Event[],
 ): CalendarEvent[] {
   // Converts a list of events.
   const fullCalendarEvents = googleCalendarEvents.map((event) =>
-    convertGoogleToFullCalendarEvent(event)
+    convertGoogleToFullCalendarEvent(event),
   );
   return fullCalendarEvents;
 }
