55439ebcd682216c09bb0d490996cf8872d093aa
Jakub Czarlinski <jakubczarlinski@gmail.com>
2024-02-12 23:08:05 +0000

    Explicitly specify flag for enabling link in home page calendar.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,11 @@ export default async function Home() {
             id="calendar"
             className="md:w-7/8 w-7/8 grid grid-cols-1 justify-self-center text-black md:place-content-center lg:w-3/4"
           >
-            <ListCalendar events={events} enableLocation={false} enableLinks/>
+            <ListCalendar
+              events={events}
+              enableLocation={false}
+              enableLinks={true}
+            />
           </div>
         </div>
 
