351e595d9425849f818d5621d1ff62f08a152144
Jakub Czarlinski <jakubczarlinski@gmail.com>
2024-02-16 19:49:14 +0000

    Make pacman its own component.

--- src/app/competition/page.tsx
+++ src/app/competition/page.tsx
@@ -2,6 +2,7 @@
 import BackButton from "@/components/back_button";
 import Button from "@/components/button";
 import defaultPage, { defaultPageWithScroll } from "@/components/default";
+import { Pacman } from "@/components/pacman";
 import CompetitionSubmission from "./form";
 
 export default function Home() {
@@ -42,7 +43,7 @@ export default function Home() {
           Good luck!
         </p>
       </div>
-      {Pacman}
+      <Pacman />
       <div className="w-full max-w-3xl justify-self-center rounded-lg border-4 border-blue-green bg-pure-white p-4 font-mono text-xl font-bold text-wai-gray shadow-sm shadow-wai-gray md:w-fit">
         Submissions are not currently open. We are in the process of testing the
         competition environment. All submission will be deleted. Please check
@@ -77,57 +78,3 @@ export default function Home() {
     </div>,
   );
 }
-
-const Pacman = (
-  <div>
-    <style>
-      {`
-        @keyframes chomp {
-          from {
-            stroke-dasharray: 157,100;
-            stroke-dashoffset: 0;
-          }
-          to {
-            stroke-dasharray: 126,100;
-            stroke-dashoffset: -15;
-          }
-        }
-        @keyframes dots {
-          from {
-            width: 95%;
-          }
-          to {
-            width: 5%;
-          }
-        }
-      `}
-    </style>
-    <div className="w-vw -ml-1 -mr-1 box-border overflow-hidden bg-wai-gray">
-      <div className="h-3 w-full rounded-xl"></div>
-      <div className="path relative -ml-[200px] -mr-[200px] mb-2 mt-2 h-20">
-        <div
-          className="float-right h-full w-full"
-          style={{
-            backgroundImage:
-              "linear-gradient(to left, #efefef 20%, transparent 0%)",
-            backgroundPosition: "center right",
-            backgroundSize: "50px 10px",
-            backgroundRepeat: "repeat-x",
-            animation: "dots 5s linear infinite",
-          }}
-        >
-          <svg className="h-full" viewBox="0 0 100 100">
-            <circle
-              className="animated-circle fill-none stroke-[#ffee00] stroke-[50%]"
-              style={{ animation: "chomp .15s linear infinite alternate" }}
-              cx="50%"
-              cy="50%"
-              r="25%"
-            ></circle>
-          </svg>
-        </div>
-      </div>
-      <div className="h-3 w-full rounded-xl"></div>
-    </div>
-  </div>
-);
