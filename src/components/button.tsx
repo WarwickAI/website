0ee7b8852c0b567638ff07b99fc38f85fcdd7a00
AnUnsocialPigeon <jdturrell@gmail.com>
2024-08-27 16:01:00 +0100

    updated to Tailwind.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,11 +7,8 @@ export default function Button(props: {
   extraClasses?: string;
   ariaLabel?: string;
 }) {
-  const classNames = `shadow-sm shadow-wai-gray bg-pure-white text-xl font-mono font-bold text-center text-wai-gray border-4 rounded-lg border-wai-gray p-4 justify-self-center`;
-  
-  // To allow for \n linebreaks
-  const textStyle = { whiteSpace: 'pre-line' };
-  
+  const classNames = `shadow-sm whitespace-pre-line shadow-wai-gray bg-pure-white text-xl font-mono font-bold text-center text-wai-gray border-4 rounded-lg border-wai-gray p-4 justify-self-center`;
+
   if (props.enabled) {
     const enabledClassNames = "hover:shadow-purple hover:border-purple hover:text-purple";
     const allClassNames = `${classNames} ${enabledClassNames} ${props.extraClasses || ""}`;
@@ -20,13 +17,12 @@ export default function Button(props: {
         href={props.href}
         className={allClassNames}
         aria-label={props.ariaLabel}
-        style={textStyle}
       >
         {props.text}
       </Link>
     );
   } else {
     const allClassNames = `${classNames} ${props.extraClasses || ""}`;
-    return <p style={textStyle} className={allClassNames}>{props.text}</p>;
+    return <p className={allClassNames}>{props.text}</p>;
   }
 }
