469a38af09bceac717ba05579624024714e26f3b
AnUnsocialPigeon <jdturrell@gmail.com>
2024-09-08 14:20:59 +0100

    Added variable tile size option

--- src/components/chess/board.tsx
+++ src/components/chess/board.tsx
@@ -9,7 +9,7 @@ import Notice from "../notice";
 // ??? -> Allow https://chess.stackexchange.com/questions/8500/alternatives-to-the-fen-notation
 
 // FEN Notation: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
-export default function ChessBoard(props: { fenString: string }) {
+export default function ChessBoard(props: { fenString: string, tileSize?: string }) {
     let game = new ChessGame(props.fenString)
 
     if (!game.validPosition)
@@ -19,29 +19,26 @@ export default function ChessBoard(props: { fenString: string }) {
 
     return (
         <div className="grid place-content-center" >
-            {/* You want axis labels? tough. I tried... Ish. */}
-            {
-                game.board.map((row, index) => (
-                    <div className="flex" key={index}>
-                        {row.map((piece, column) => (
-                            <div
-                                className={`${boardStyle["tile" + ((index + column) % 2)]}`}
-                                style={{ '--tile-size': '80px' } as React.CSSProperties}
-                                key={column}
-                            >
-                                {piece ? (
-                                    <Piece
-                                        pieceColour={piece.colour}
-                                        pieceType={piece.name}
-                                    />
-                                ) : (
-                                    <div className={boardStyle.blankTile} />
-                                )}
-                            </div>
-                        ))}
-                    </div>
-                ))
-            }
+            {game.board.map((row, index) => (
+                <div className="flex" key={index}>
+                    {row.map((piece, column) => (
+                        <div
+                            className={`${boardStyle["tile" + ((index + column) % 2)]}`}
+                            style={{ '--tile-size': props.tileSize || '80px' } as React.CSSProperties}
+                            key={column}
+                        >
+                            {piece ? (
+                                <Piece
+                                    pieceColour={piece.colour}
+                                    pieceType={piece.name}
+                                />
+                            ) : (
+                                <div className={boardStyle.blankTile} />
+                            )}
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div >
     );
 } 
