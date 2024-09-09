f990ada59ab536d657d1485b365c021b1feef3e8
AnUnsocialPigeon <jdturrell@gmail.com>
2024-09-09 14:43:21 +0100

    Supid Next.js won't let you look at the url params if there isn't a fallback.... Which then has to also be in a strictly client-side component, not page. Why.

--- src/app/competition/chess/page.tsx
+++ src/app/competition/chess/page.tsx
@@ -1,22 +1,16 @@
-"use client"
+import BackButton from '@/components/back_button';
+import ChessUrlWrapper from '@/components/chess/chess_from_url';
+import defaultPage from '@/components/default';
+import { Suspense } from 'react';
 
-import { useSearchParams } from 'next/navigation';
-import BackButton from "@/components/back_button";
-import ChessBoard from "@/components/chess/board";
-import defaultPage from "@/components/default";
 
 export default function Home() {
-    const searchParams = useSearchParams();
-    const fenString = searchParams.get('fenString');
-
-    // Fallback to default position
-    const defaultFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-    const boardFEN = fenString ? fenString : defaultFEN;
-
     return defaultPage(
-        <div>
-            <ChessBoard fenString={boardFEN} />
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ChessUrlWrapper />
+            </Suspense>
             <BackButton />
-        </div>,
+        </>
     );
 }
