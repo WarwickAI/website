3a401fd2f6846a1c03da678c0b86cec8bc034994
AnUnsocialPigeon <jdturrell@gmail.com>
2024-10-21 09:30:55 +0100

    Added 1-day caching for all images served by Vercel. (This can be altered per path when required; for now, a global image cache is good enough.).

--- next.config.js
+++ next.config.js
@@ -18,6 +18,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: "/images/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400, stale-while-revalidate=59",
+          },
+        ],
+      },
     ];
   },
 };
