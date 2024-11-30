a15c5a3c08a5b7d28e409fcd18c039da9779cf20
AnUnsocialPigeon <jdturrell@gmail.com>
2024-11-30 22:37:27 +0000

    Updating trying to fix twitch stuff?

--- src/components/twtich/embed.tsx
+++ src/components/twtich/embed.tsx
@@ -5,13 +5,14 @@ export default function TwitchEmbed(props: {
   height?: string;
   width?: string;
 }) {
+  // Accoding to twitch TOS: 400x300 is the minimum player size! 
   return (
     <div className="grid grid-cols-1 place-self-center">
       <p className="text-center font-mono text-lg text-wai-gray">
         Our Benji is currently live!
       </p>
       <iframe
-        src={`https://player.twitch.tv/?channel=${props.channel}&parent=${process.env.NODE_ENV === "development" ? 'localhost' : 'www.warwick.ai'}`}
+        src={`https://player.twitch.tv/?channel=${props.channel}&parent=${process.env.NODE_ENV === "development" ? 'localhost' : 'warwick.ai'}`}
         height={props.height ?? "420"}
         width={props.width ?? "720"}
         allowFullScreen={true}
