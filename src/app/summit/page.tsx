0c063160231548f84d380bed86046b0781868415
AnUnsocialPigeon <jdturrell@gmail.com>
2025-03-12 22:54:31 +0000

    Taken down summit page, it's over. Also updated README to be a bit more helpful, hopefully.

--- src/app/summit/page.tsx
+++ src/app/summit/page.tsx
@@ -13,7 +13,7 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  const enablePage = true;
+  const enablePage = false;
   if (!enablePage) {
     return defaultPage(
       <div>
